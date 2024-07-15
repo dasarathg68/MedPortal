1b73a3737e2de8e666a5691018df4637d29e51c4
Dasarath G <dasarathg68@gmail.com>
2024-07-15 22:39:35 +0530

    ci/cd: backend url

--- app/src/utils/axios.jsx
+++ app/src/utils/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import data from "./config.json";
 const instance = axios.create({
-  baseURL: data.development,
+  baseURL: data.production,
 });
 export default instance;
